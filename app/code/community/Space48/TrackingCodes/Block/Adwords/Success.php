372ee720ec22bb6752d19e05b2e7f4e8ce26f6f5
DanCarlyon <dan.carlyon@space48.com>
2016-02-08 15:52:33 +0000

    added a check for if backend  order value is set, if not set, make magento return the value

--- app/code/community/Space48/TrackingCodes/Block/Adwords/Success.php
+++ app/code/community/Space48/TrackingCodes/Block/Adwords/Success.php
@@ -1,5 +1,12 @@
 <?php
 class Space48_TrackingCodes_Block_Adwords_Success extends Space48_TrackingCodes_Block_Adwords_Abstract
 {
-
+    public function getTotalValue()
+    {
+        if(!$this->getFieldValue('conversionvalue')) {
+            return $this->getCheckoutHelper()->getLastOrder()->getGrandTotal();
+        } else {
+            return $this->getFieldValue('conversionvalue');
+        }
+    }
 }
